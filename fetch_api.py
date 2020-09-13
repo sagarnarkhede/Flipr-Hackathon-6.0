d05c3c8391bfb54272b120b92c3954b6d99aab54
hemantghuge <hemantgghuge@gmail.com>
2020-09-13 17:53:50 +0530

    Added notifications fetch api function

--- fetch_api.py
+++ fetch_api.py
@@ -2,6 +2,7 @@ import requests
 
 
 contacts_url = 'https://api.rootnet.in/covid19-in/contacts'
+notifications_url = 'https://api.rootnet.in/covid19-in/notifications'
 
 def fetch_data_contacts(url):
     result = requests.get(url)
@@ -15,13 +16,36 @@ def fetch_data_contacts(url):
     #print(data['data']['contacts'])
     #print(type(data)) data is in a form of dictionary
 
+    regionals_dict = {}
     primary_contacts = data['data']['contacts']['primary']
     regional_contacts = data['data']['contacts']['regional']
     #print(regional_contacts)
     #print(type(regional_contacts))
     print('State Name'+'\t\t'+'Helpline Number')
     for item in regional_contacts:
-        print(item['loc']+'\t\t'+item['number'])
+        #print(item['loc']+'\t\t'+item['number'])
+        regionals_dict[item['loc']]=item['number']
+
+    return regionals_dict
+
+def fetch_data_notifications(url):
+    result = requests.get(url)
+    data = result.json()
+
+    date_dict = {}
+    notification_dict = {}
+    notification = data['data']['notifications']
+    i = 0
+    for item in notification:
+        title = item['title']
+        if len(title.split()[0]) == 10 and title[6:8]=='20':
+            date = title[0:10]
+            date_dict[str(i)] = date
+            notification_dict[title[11:]] = item['link']
+            i+=1
+
+    return (date_dict, notification_dict)
 
 
-fetch_data_contacts(contacts_url)
+#print(fetch_data_contacts(contacts_url))
+print(fetch_data_notifications(notifications_url))
